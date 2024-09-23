608f20a151abbc8bfdcb18fb4d4a4ad4f9bee95d
Julien Marrec <julien.marrec@gmail.com>
2024-09-23 13:45:14 +0200

    Redo the links to the repo in getReleaseInfo

--- openstudioreleases/scripts/getReleaseInfo.ts
+++ openstudioreleases/scripts/getReleaseInfo.ts
@@ -23,10 +23,21 @@ const run = async () => {
     return version && semver.gt(version, "1.6.9");
   });
 
+  const PR_REGEX = /in https:\/\/github\.com\/openstudiocoalition\/OpenStudioApplication\/pull\/(\d+)/gm;
+  const PR_CHANGELOG = /(https:\/\/github\.com\/openstudiocoalition\/OpenStudioApplication\/compare\/(v[\d\.]+)\.\.\.(v[\d\.]+))/gm;
+  const GITHUB_USER_REGEX = /by @(\w+) /gm;
+  const GITHUB_USER_REGEX2 = /@(\w+) made/gm;
+
   const allReleases = filteredReleases.map(r => ({
     name: r.name,
     tag_name: r.tag_name,
-    body: r.body.replaceAll(':heavy_check_mark:', '✔️').replaceAll(':heavy_plus_sign:', '➕'),
+    body: r.body
+           .replaceAll(':heavy_check_mark:', '✔️')
+           .replaceAll(':heavy_plus_sign:', '➕')
+           .replaceAll(PR_REGEX, 'in [#$1](https://github.com/openstudiocoalition/OpenStudioApplication/pull/$1)')
+           .replaceAll(GITHUB_USER_REGEX, 'by [@$1](https://github.com/$1) ')
+           .replaceAll(GITHUB_USER_REGEX2, '[@$1](https://github.com/$1) made')
+           .replaceAll(PR_CHANGELOG, '[$2...$3]($1)'),
     assets: r.assets.map(a => ({
       url: a.url,
       name: a.name,
